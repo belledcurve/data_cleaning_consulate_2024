8f7b62a887e505a1c7ab4f1887bc2a0ead0643e1
SAMUEL LEE <samuellee@nyu.edu>
2024-02-29 19:20:05 -0500

    Update clean_number.py

--- clean_number.py
+++ clean_number.py
@@ -7,8 +7,8 @@ def clean_number(number : str) -> str:
     
     if (re.search('\D', number[0])!=None):
         # the longest global exension is 2
-        global_extension = re.search("^\D\d{1,2}", number)
-        if (len(global_extension.group()) > 2):
+        global_extension = re.search("^\D\d{0,2}", number)
+        if (len(global_extension.group()) > 3):
             rest = number;
         else:
             clean_number += '+' + global_extension.group()[1:] + ' ';
@@ -18,6 +18,20 @@ def clean_number(number : str) -> str:
         rest = number
     
     rest_of_number = re.findall('\d+',rest);
-    clean_number += ' '.join(rest_of_number);
+    clean_number += ''.join(rest_of_number);
 
     return clean_number;
+
+
+
+import pandas as pd;
+
+testcase_numbers = pd.read_csv('../testcases/phone_numbers.csv');
+testcase_numbers['clean_no'] = testcase_numbers.phone.apply(clean_number);
+
+f = open('phone_numbers_test_case.csv', 'w');
+for numbers in testcase_numbers.clean_no:
+    f.write(numbers + '\n');
+
+f.close()
+
